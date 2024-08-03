643cbf65823757f8bf3bec9041ba91359e658e97
amitkumar7245 <amitkdna@gmail.com>
2024-08-03 13:24:05 +0530

    userauth

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +19,19 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
+Route::get('/userlogin', function () {
+    return view('frontend.userauth.login');
+});
+Route::get('/userregistration', function () {
+    return view('frontend.userauth.registration');
+});
+
+Route::get('/forgot-password', function () {
+    return view('frontend.userauth.forgot_password');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
 
